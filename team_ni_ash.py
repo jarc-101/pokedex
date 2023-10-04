61f2131860eb85801ac9170b99e49a6cc1798dcb
jarc-101 <arroyojohnarchel99@gmail.com>
2023-10-04 18:01:42 +0800

    Python update

--- team_ni_ash.py
+++ team_ni_ash.py
@@ -11,17 +11,18 @@ def search_pokemon(name):
 def add_pokemon(pokemon):
   Name = f"Name: {pokemon['name'].capitalize()}"
   hp = f"HP: {pokemon['stats'][0]['base_stat']}"
+  print(Name)
+  print(hp)
+  add_items(pokemon)
+  
+def add_items(pokemon):
   items = f"Held Items: {pokemon['held_items']}"
-
   if len(pokemon['held_items']) == 0:
     items_print = "Held Item: No item held"
   else:
     items = [item['item']['name'] for item in pokemon['held_items']]
     items = random.sample(items,1)
     items_print = f"Held Item: {items[0]}"
-
-  print(Name)
-  print(hp)
   print(items_print)
   get_moves(pokemon)
 
@@ -30,22 +31,34 @@ def get_moves(pokemon):
   moveset = random.sample(moves,4)
   print(f"Moveset: {moveset[0]}, {moveset[1]}, {moveset[2]}, {moveset[3]}\n")
 
-if __name__ == "__main__":  
-  # choose_you()
-  search_pokemon(sys.argv[1])
-  search_pokemon(sys.argv[2])
-  search_pokemon(sys.argv[3])
-  search_pokemon(sys.argv[4])
-  search_pokemon(sys.argv[5])
-  search_pokemon(sys.argv[6])
+# if __name__ == "__main__":  
+#   search_pokemon(sys.argv[1])
+#   search_pokemon(sys.argv[2])
+#   search_pokemon(sys.argv[3])
+#   search_pokemon(sys.argv[4])
+#   search_pokemon(sys.argv[5])
+#   search_pokemon(sys.argv[6])
 
-
-
-
+if __name__ == "__main__":
   poke_num = 0
   team = {}
   while poke_num != 6:
-    poke_name = input("Choose your Pokemon: ").capitalize()
-    team[poke_name] = "1"
+    poke_name = input("Choose your Pokemon: ").lower()
+    search_pokemon(poke_name)
+    team[poke_name] = None
     poke_num +=1
-  print(team)
+
+print("Gotta catch em' all")
+print(team)
+
+
+
+# poke_num = 0
+# team = {}
+# while poke_num != 6:
+#   poke_name = input("Choose your Pokemon: ").lower()  
+#   if __name__ == "__main__":  
+#     search_pokemon(poke_name)
+#   team[poke_name] = None
+#   poke_num +=1
+# print(team)
